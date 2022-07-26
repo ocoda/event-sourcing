7d1549c6472c02c26bff070d25907d8b25bc5df5
Dries Hooghe <dries@drieshooghe.com>
2022-07-26 17:54:25 +0200

    :sparkles: add an abstract value-object class

---
+++ lib/value-object.ts
@@ -0,0 +1,24 @@
+interface ValueObjectProps {
+  [index: string]: any;
+}
+
+export abstract class ValueObject<T extends ValueObjectProps> {
+  public readonly props: T;
+
+  protected constructor(props: T) {
+    this.props = Object.freeze(props);
+  }
+
+  public equals(other: ValueObject<T>): boolean {
+    if (this.constructor !== other.constructor) {
+      return false;
+    }
+
+    return (
+      Object.keys(this.props).length === Object.keys(other.props).length &&
+      Object.keys(this.props).every(
+        (key) => this.props[key] === other.props[key],
+      )
+    );
+  }
+}
