7cfa366ad3dc8f97d9802d44d3506b0ff721a3be
Dries Hooghe <dries@drieshooghe.com>
2022-07-26 17:56:24 +0200

    :truck: move application exceptions into their own directory

--- lib/exceptions/index.ts
+++ lib/exceptions/index.ts
@@ -1,6 +1,1 @@
-export * from './command-handler-not-found.exception';
-export * from './event-handler-not-found.exception';
-export * from './invalid-command-handler.exception';
-export * from './invalid-event-handler.exception';
-export * from './invalid-query-handler.exception';
-export * from './query-handler-not-found.exception';
+export * from './application';
