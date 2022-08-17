727074515695c70d357442c88aa02f2c202d0cee
Dries Hooghe <dries@drieshooghe.com>
2022-08-17 16:55:50 +0200

    :sparkles: add event-streams

---
+++ lib/event-stream.spec.ts
@@ -0,0 +1,24 @@
+import { Aggregate } from './aggregate';
+import { EventStream } from './event-stream';
+import { Id } from './id';
+import { IEvent } from './interfaces';
+
+describe(Aggregate, () => {
+  class Account extends Aggregate {}
+  class AccountId extends Id {}
+
+  it('should create an event-stream from an Aggregate class', () => {
+    const accountId = AccountId.generate();
+    const eventStream = EventStream.for(Account, accountId);
+
+    expect(eventStream).toBe(`Account-${accountId.value}`);
+  });
+
+  it('should create an event-stream from an Aggregate instance', () => {
+    const account = new Account();
+    const accountId = AccountId.generate();
+    const eventStream = EventStream.for(account, accountId);
+
+    expect(eventStream).toBe(`Account-${accountId.value}`);
+  });
+});
---
+++ lib/event-stream.ts
@@ -0,0 +1,10 @@
+import { Type } from '@nestjs/common';
+import { Aggregate } from './aggregate';
+import { Id } from './id';
+
+export class EventStream extends String {
+  static for(aggregate: Aggregate | Type<Aggregate>, id: Id) {
+    const className = aggregate instanceof Function ? aggregate.name : aggregate.constructor.name;
+    return `${className}-${id.value}`;
+  }
+}
