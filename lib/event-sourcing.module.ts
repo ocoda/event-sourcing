357a07beee3c19d6d5bcf012a4d956d18bcd291e
Dries Hooghe <dries@drieshooghe.com>
2022-08-21 11:20:28 +0200

    :sparkles: make the event-sourcing module options required

--- lib/event-sourcing.module.ts
+++ lib/event-sourcing.module.ts
@@ -3,28 +3,31 @@ import { DiscoveryModule } from '@nestjs/core';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { CommandBus } from './command-bus';
 import { EventPublisher } from './event-publisher';
+import { EventSerializer } from './event-serializer';
 import { EventStore } from './event-store';
 import { HandlersLoader } from './handlers.loader';
-import { InMemoryEventStore } from './integration/in-memory';
+import { DefaultEventSerializer } from './helpers/default-event-serializer';
+import { InMemoryEventStore } from './integration/event-store';
 import { EventSourcingModuleOptions } from './interfaces';
 import { QueryBus } from './query-bus';
 
 @Module({})
 export class EventSourcingModule {
-  static forRoot(options?: EventSourcingModuleOptions): DynamicModule {
+  static forRoot(options: EventSourcingModuleOptions): DynamicModule {
     const providers: Provider[] = [
       CommandBus,
       QueryBus,
       HandlersLoader,
       EventPublisher,
       { provide: EventStore, useClass: InMemoryEventStore },
+      { provide: EventSerializer, useClass: DefaultEventSerializer },
     ];
 
     return {
       module: EventSourcingModule,
       imports: [DiscoveryModule, EventEmitterModule.forRoot()],
       providers,
-      exports: [CommandBus, QueryBus, EventPublisher],
+      exports: [CommandBus, QueryBus, EventPublisher, EventStore],
     };
   }
 }
