91e5289541a7f20d6227b261ec2535b8a1f4d803
Dries Hooghe <dries@drieshooghe.com>
2022-08-21 11:13:05 +0200

    :label: add events to register to the event-sourcing module options

--- lib/interfaces/event-sourcing-options.interface.ts
+++ lib/interfaces/event-sourcing-options.interface.ts
@@ -1,1 +1,6 @@
-export interface EventSourcingModuleOptions {}
+import { Type } from '@nestjs/common';
+import { IEvent, IEventSerializer } from './events';
+
+export interface EventSourcingModuleOptions {
+  events: [Type<IEvent>, IEventSerializer?][];
+}
