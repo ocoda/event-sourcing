9cc204479bd2771e201804235b9ad4c3f3ceca33
Dries Hooghe <dries@drieshooghe.com>
2022-08-25 22:32:23 +0200

    :recycle: refactor the snapshot-envelope

--- lib/models/snapshot-envelope.spec.ts
+++ lib/models/snapshot-envelope.spec.ts
@@ -1,13 +1,21 @@
 import { IEvent, ISnapshot } from '../interfaces';
+import { Aggregate } from './aggregate';
 import { Id } from './id';
 import { SnapshotEnvelope } from './snapshot-envelope';
 
 describe(SnapshotEnvelope, () => {
   const now = new Date();
 
-  class FooId extends Id {}
-  interface FooSnapshot extends ISnapshot {
-    bar: string;
+  class AccountId extends Id {}
+  class Account extends Aggregate {
+    constructor(
+      public id: AccountId,
+      public balance: number,
+      public openedOn: Date,
+      public closedOn: Date,
+    ) {
+      super();
+    }
   }
 
   beforeAll(() =>
@@ -15,14 +23,19 @@ describe(SnapshotEnvelope, () => {
   );
 
   it('should create a snapshot-envelope', () => {
-    const fooId = FooId.generate();
-    const fooSnapshot: FooSnapshot = { bar: 'bar' };
+    const accountId = AccountId.generate();
+    const accountSnapshot: ISnapshot<Account> = {
+      id: accountId,
+      balance: 0,
+      openedOn: new Date(),
+      closedOn: undefined,
+    };
 
-    const envelope = SnapshotEnvelope.new(fooId, 1, fooSnapshot);
+    const envelope = SnapshotEnvelope.new(accountId, 1, accountSnapshot);
 
     expect(envelope.snapshotId).toBeDefined();
-    expect(envelope.payload).toEqual({ bar: 'bar' });
-    expect(envelope.metadata.aggregateId).toEqual(fooId.value);
+    expect(envelope.payload).toEqual(accountSnapshot);
+    expect(envelope.metadata.aggregateId).toEqual(accountId.value);
     expect(envelope.metadata.sequence).toBe(1);
     expect(envelope.metadata.registeredOn).toEqual(now.valueOf());
   });
--- lib/models/snapshot-envelope.ts
+++ lib/models/snapshot-envelope.ts
@@ -1,23 +1,28 @@
 import { randomUUID } from 'crypto';
 import { Id } from './id';
-import { SnapshotEnvelopeMetadata } from '../interfaces';
+import { ISnapshot, SnapshotEnvelopeMetadata } from '../interfaces';
+import { Aggregate } from './aggregate';
 
-export class SnapshotEnvelope {
+export class SnapshotEnvelope<A extends Aggregate> {
   public readonly snapshotId: string;
-  readonly payload: unknown;
+  readonly payload: ISnapshot<A>;
   readonly metadata: SnapshotEnvelopeMetadata;
 
-  private constructor(aggregateId: string, sequence: number, payload: unknown) {
+  private constructor(
+    aggregateId: string,
+    sequence: number,
+    payload: ISnapshot<A>,
+  ) {
     this.snapshotId = randomUUID();
     this.payload = payload;
     this.metadata = { aggregateId, sequence, registeredOn: Date.now() };
   }
 
-  static new(
+  static new<A extends Aggregate>(
     aggregateId: Id,
     sequence: number,
-    payload: unknown,
-  ): SnapshotEnvelope {
+    payload: ISnapshot<A>,
+  ): SnapshotEnvelope<A> {
     return new SnapshotEnvelope(aggregateId.value, sequence, payload);
   }
 }
