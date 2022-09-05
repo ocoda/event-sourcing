23da046319d7232a4e31cc12dbbaf13835210cdb
Dries Hooghe <dries@drieshooghe.com>
2022-09-05 14:36:19 +0200

    :sparkles: add exceptions

---
+++ lib/exceptions/application/missing-snapshot-metadata.exception.ts
@@ -0,0 +1,7 @@
+export class MissingSnapshotMetadataException extends Error {
+  constructor(aggregate: Function) {
+    super(
+      `Missing snapshot metadata exception for ${aggregate.constructor} (missing @Snapshot() decorator?)`,
+    );
+  }
+}
---
+++ lib/exceptions/application/unregistered-aggregate.exception.ts
@@ -0,0 +1,7 @@
+export class UnregisteredAggregateException extends Error {
+  constructor(aggregate: string) {
+    super(
+      `Aggregate '${aggregate}' is not registered. Register it in the EventSourcingModule.`,
+    );
+  }
+}
