db69a5315f1fcb5e6451d14d1439c8e60d0f6650
Dries Hooghe <dries@drieshooghe.com>
2022-09-06 14:45:47 +0200

    :sparkles: allow event-envelopes to be recreated

--- lib/models/event-envelope.ts
+++ lib/models/event-envelope.ts
@@ -3,19 +3,22 @@ import { EventEnvelopeMetadata } from '../interfaces';
 import { Id } from './id';
 
 export class EventEnvelope {
-	public readonly eventId: string;
-	public readonly eventName: string;
-	readonly payload: Record<string, any>;
-	readonly metadata: EventEnvelopeMetadata;
-
-	private constructor(aggregateId: string, sequence: number, eventName: string, payload: unknown) {
-		this.eventId = randomUUID();
-		this.eventName = eventName;
-		this.payload = payload;
-		this.metadata = { aggregateId, sequence, occurredOn: Date.now() };
-	}
+	private constructor(
+		public readonly eventId: string,
+		public readonly eventName: string,
+		readonly payload: Record<string, any>,
+		readonly metadata: EventEnvelopeMetadata,
+	) {}
 
 	static new(aggregateId: Id, sequence: number, eventName: string, payload: unknown): EventEnvelope {
-		return new EventEnvelope(aggregateId.value, sequence, eventName, payload);
+		return new EventEnvelope(randomUUID(), eventName, payload, {
+			aggregateId: aggregateId.value,
+			sequence,
+			occurredOn: Date.now(),
+		});
+	}
+
+	static from(eventId: string, eventName: string, payload: unknown, metadata: EventEnvelopeMetadata): EventEnvelope {
+		return new EventEnvelope(eventId, eventName, payload, metadata);
 	}
 }
