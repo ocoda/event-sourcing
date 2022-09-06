9cbd3708a7237a30a41640097b3903287b439328
Dries Hooghe <dries@drieshooghe.com>
2022-09-06 14:46:07 +0200

    :sparkles: store the aggregate as the event-stream subject

--- lib/models/event-stream.ts
+++ lib/models/event-stream.ts
@@ -3,10 +3,14 @@ import { Aggregate } from './aggregate';
 import { Id } from './id';
 
 export class EventStream<A extends Aggregate = Aggregate> {
-	private constructor(private aggregate: Type<A>, private id: Id) {}
+	private constructor(private _subject: Type<A>, private _id: Id) {}
+
+	get subject(): string {
+		return this._subject.name;
+	}
 
 	get name(): string {
-		return `${this.aggregate.name}-${this.id.value}`;
+		return `${this._subject.name}-${this._id.value}`;
 	}
 
 	static for<A extends Aggregate<any> = Aggregate<any>>(aggregate: A | Type<A>, id: Id): EventStream<A> {
