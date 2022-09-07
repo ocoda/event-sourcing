314515ba0f4f5a8bca728253d33ad9655bd6ab16
Dries Hooghe <dries@drieshooghe.com>
2022-09-07 14:54:40 +0200

    :sparkles: equip the EventMap to perform the event (de)serialization

--- lib/event-map.spec.ts
+++ lib/event-map.spec.ts
@@ -1,32 +1,22 @@
 import { EventName } from './decorators';
 import { EventMap } from './event-map';
 import { MissingEventMetadataException, UnregisteredEventException } from './exceptions';
-import { DefaultEventSerializer } from './helpers';
-import { IEvent, IEventSerializer } from './interfaces';
-import { Id } from './models';
+import { DefaultEventSerializer } from "./helpers";
+import { IEvent, IEventPayload, IEventSerializer } from './interfaces';
 
 describe(EventMap, () => {
+	let now = new Date();
+
 	@EventName('account-opened')
 	class AccountOpenedEvent implements IEvent {
 		constructor(public readonly opened: Date) {}
 	}
 
-	@EventName('account-credited')
-	class AccountCreditedEvent implements IEvent {
-		constructor(public readonly amount: number) {}
-	}
-
-	@EventName('account-debited')
-	class AccountDebitedEvent implements IEvent {
-		constructor(public readonly amount: number) {}
-	}
-
-	@EventName('account-closed')
-	class AccountClosedEvent implements IEvent {
-		constructor(public readonly closed: Date) {}
-	}
-
 	class UnregisteredEvent implements IEvent {}
+
+	beforeAll(() => jest.useFakeTimers({ now }));
+
+	afterAll(() => jest.useRealTimers());
 
 	it('throws when registering an event without an event-name', () => {
 		class FooCreatedEvent implements IEvent {}
@@ -42,43 +32,47 @@ describe(EventMap, () => {
 
 		expect(eventMap.has('account-opened')).toBe(true);
 		expect(eventMap.has(AccountOpenedEvent)).toBe(true);
+		expect(eventMap.has(new AccountOpenedEvent(new Date()))).toBe(true);
+
 		expect(eventMap.has('unregistered-event')).toBe(false);
 		expect(eventMap.has(UnregisteredEvent)).toBe(false);
 	});
 
-	it('returns the constructor of a registered event by its name', () => {
+	it('returns the constructor of a registered event by its name or an instance', () => {
 		const eventMap = new EventMap();
 		eventMap.register(AccountOpenedEvent);
-		eventMap.register(AccountClosedEvent);
 
 		expect(eventMap.getConstructor('account-opened')).toBe(AccountOpenedEvent);
-		expect(eventMap.getConstructor('account-closed')).toBe(AccountClosedEvent);
+		expect(eventMap.getConstructor(new AccountOpenedEvent(new Date()))).toBe(AccountOpenedEvent);
 	});
 
-	it('throws when trying to get the constructor of an unregistered event by its name', () => {
+	it('throws when trying to get the constructor of an unregistered event by its name or an instance', () => {
 		const eventMap = new EventMap();
 
 		expect(() => eventMap.getConstructor('unregistered-event')).toThrowError(
 			new UnregisteredEventException('unregistered-event'),
 		);
+		expect(() => eventMap.getConstructor(new UnregisteredEvent())).toThrowError(
+			new UnregisteredEventException('UnregisteredEvent'),
+		);
 	});
 
-	it('returns the name of a registered event by its constructor', () => {
+	it('returns the name of a registered event by its constructor or an instance', () => {
 		const eventMap = new EventMap();
 		eventMap.register(AccountOpenedEvent);
-		eventMap.register(AccountClosedEvent);
 
 		expect(eventMap.getName(AccountOpenedEvent)).toBe('account-opened');
-		expect(eventMap.getName(AccountClosedEvent)).toBe('account-closed');
+		expect(eventMap.getName(new AccountOpenedEvent(new Date()))).toBe('account-opened');
 	});
 
-	it('throws when trying to get the name of an unregistered event by its constructor', () => {
+	it('throws when trying to get the name of an unregistered event by its constructor or an instance', () => {
 		const eventMap = new EventMap();
 
-		expect(() => eventMap.getName(UnregisteredEvent)).toThrowError(new UnregisteredEventException('UnregisteredEvent'));
+		expect(() => eventMap.getName(UnregisteredEvent)).toThrowError(new UnregisteredEventException(UnregisteredEvent));
+		expect(() => eventMap.getName(new UnregisteredEvent())).toThrowError(new UnregisteredEventException(new UnregisteredEvent()));
 	});
 
-	it('returns the serializer of a registered event by its name', () => {
+	it('returns the serializer of a registered event by its name, constructor or an instance', () => {
 		const customEventSerializer: IEventSerializer<AccountOpenedEvent> = {
 			serialize: ({ opened }: AccountOpenedEvent) => ({
 				opened: opened.toISOString(),
@@ -90,68 +84,29 @@ describe(EventMap, () => {
 		eventMap.register(AccountOpenedEvent, customEventSerializer);
 
 		expect(eventMap.getSerializer('account-opened')).toBe(customEventSerializer);
-	});
-
-	it('returns the serializer of a registered event by its constructor', () => {
-		const customEventSerializer: IEventSerializer<AccountOpenedEvent> = {
-			serialize: ({ opened }: AccountOpenedEvent) => ({
-				opened: opened.toISOString(),
-			}),
-			deserialize: ({ opened }: { opened: string }) => new AccountOpenedEvent(new Date(opened)),
-		};
-
-		const eventMap = new EventMap();
-		eventMap.register(AccountOpenedEvent, customEventSerializer);
-
 		expect(eventMap.getSerializer(AccountOpenedEvent)).toBe(customEventSerializer);
+		expect(eventMap.getSerializer(new AccountOpenedEvent(new Date()))).toBe(customEventSerializer);
 	});
 
-	it('creates event-envelopes', () => {
-		class AccountId extends Id {}
-
+	it('serializes a registered event', () => {
 		const eventMap = new EventMap();
 		eventMap.register(AccountOpenedEvent, DefaultEventSerializer.for(AccountOpenedEvent));
-		eventMap.register(AccountCreditedEvent, DefaultEventSerializer.for(AccountCreditedEvent));
-		eventMap.register(AccountDebitedEvent, DefaultEventSerializer.for(AccountDebitedEvent));
-		eventMap.register(AccountClosedEvent, DefaultEventSerializer.for(AccountClosedEvent));
 
-		const accountId = AccountId.generate();
+		const event = new AccountOpenedEvent(new Date());
+		const payload = eventMap.serializeEvent<AccountOpenedEvent>(event)
 
-		const openedEvent = new AccountOpenedEvent(new Date());
-		const creditedEvent = new AccountCreditedEvent(100);
-		const debitedEvent = new AccountDebitedEvent(50);
-		const closedEvent = new AccountClosedEvent(new Date());
+		expect(payload).toBeInstanceOf(Object)
+		expect(payload).toEqual(event)
+	})
 
-		const envelopes = eventMap.createEnvelopes(accountId, 4, [openedEvent, creditedEvent, debitedEvent, closedEvent]);
+	it('deserializes a registered event', () => {
+		const eventMap = new EventMap();
+		eventMap.register(AccountOpenedEvent, DefaultEventSerializer.for(AccountOpenedEvent));
 
-		expect(envelopes).toHaveLength(4);
+		const payload = { opened: new Date() };
+		const event = eventMap.deserializeEvent<AccountOpenedEvent>('account-opened', payload);
 
-		expect(envelopes[0].eventId).toBeDefined();
-		expect(envelopes[0].eventName).toBe('account-opened');
-		expect(envelopes[0].payload).toEqual({ opened: openedEvent.opened });
-		expect(envelopes[0].metadata.aggregateId).toBe(accountId.value);
-		expect(envelopes[0].metadata.sequence).toBe(1);
-		expect(envelopes[0].metadata.occurredOn).toBeDefined();
-
-		expect(envelopes[1].eventId).toBeDefined();
-		expect(envelopes[1].eventName).toBe('account-credited');
-		expect(envelopes[1].payload).toEqual({ amount: creditedEvent.amount });
-		expect(envelopes[1].metadata.aggregateId).toBe(accountId.value);
-		expect(envelopes[1].metadata.sequence).toBe(2);
-		expect(envelopes[1].metadata.occurredOn).toBeDefined();
-
-		expect(envelopes[2].eventId).toBeDefined();
-		expect(envelopes[2].eventName).toBe('account-debited');
-		expect(envelopes[2].payload).toEqual({ amount: debitedEvent.amount });
-		expect(envelopes[2].metadata.aggregateId).toBe(accountId.value);
-		expect(envelopes[2].metadata.sequence).toBe(3);
-		expect(envelopes[2].metadata.occurredOn).toBeDefined();
-
-		expect(envelopes[3].eventId).toBeDefined();
-		expect(envelopes[3].eventName).toBe('account-closed');
-		expect(envelopes[3].payload).toEqual({ closed: closedEvent.closed });
-		expect(envelopes[3].metadata.aggregateId).toBe(accountId.value);
-		expect(envelopes[3].metadata.sequence).toBe(4);
-		expect(envelopes[3].metadata.occurredOn).toBeDefined();
-	});
+		expect(event).toBeInstanceOf(AccountOpenedEvent)
+		expect(event).toEqual(event)
+	})
 });
--- lib/event-map.ts
+++ lib/event-map.ts
@@ -1,22 +1,27 @@
 import { Injectable, Type } from '@nestjs/common';
 import { EVENT_NAME_METADATA } from './decorators';
 import { MissingEventMetadataException, UnregisteredEventException } from './exceptions';
-import { IEvent, IEventSerializer } from './interfaces';
-import { EventEnvelope, Id } from './models';
+import { IEvent, IEventPayload, IEventSerializer } from './interfaces';
 
 export type EventSerializerType = Type<IEventSerializer<IEvent>>;
 
-interface EventData {
-	name: string;
-	cls: Type<IEvent>;
-	serializer?: IEventSerializer<IEvent>;
+type IEventName = string;
+type IEventConstructor<E extends IEvent = IEvent> = Type<E>;
+type IEventInstance<E extends IEvent = IEvent> = E;
+
+interface IEventData<E extends IEvent> {
+	name: IEventName;
+	cls: IEventConstructor<E>;
+	serializer?: IEventSerializer<E>;
 }
+
+export type IEventMapTarget<E extends IEvent = IEvent> = IEventName | IEventConstructor<E> | IEventInstance<E>;
 
 @Injectable()
 export class EventMap {
-	private readonly eventMap: Set<EventData> = new Set();
+	private readonly eventMap: Set<IEventData<unknown>> = new Set();
 
-	public register<E extends Type<IEvent>>(cls: E, serializer?: IEventSerializer): void {
+	public register<E extends IEvent>(cls: IEventConstructor<E>, serializer?: IEventSerializer): void {
 		const name: string = Reflect.getMetadata(EVENT_NAME_METADATA, cls);
 		if (!name) {
 			throw new MissingEventMetadataException(cls);
@@ -25,55 +30,56 @@ export class EventMap {
 		this.eventMap.add({ name, cls, serializer });
 	}
 
-	private get<E extends Type<IEvent>>(target: string | E): EventData {
+	private get<E extends IEvent>(target: IEventMapTarget<E>): IEventData<E> {
 		for (const helper of this.eventMap) {
 			if (typeof target === 'string' && target === helper.name) {
-				return helper;
+				return helper as IEventData<E>;
+			}
+			if (typeof target === 'object' && target.constructor === helper.cls) {
+				return helper as IEventData<E>;
 			}
 			if (typeof target === 'function' && target === helper.cls) {
-				return helper;
+				return helper as IEventData<E>;
 			}
 		}
 	}
 
-	public has<E extends Type<IEvent>>(event: string | E): boolean {
+	public has<E extends IEvent>(event: IEventMapTarget<E>): boolean {
 		return this.get(event) !== undefined;
 	}
 
-	public createEnvelopes(aggregateId: Id, aggregateVersion: number, events: IEvent[]): EventEnvelope[] {
-		let sequence = aggregateVersion - events.length + 1;
-		return events.map((event) => {
-			const { name, serializer } = this.get(event.constructor as Type<IEvent>);
-			const envelope = EventEnvelope.new(aggregateId, sequence, name, serializer?.serialize(event) || event);
-
-			sequence++;
-
-			return envelope;
-		});
+	public serializeEvent<E extends IEvent>(event: IEventInstance<E>): IEventPayload<E> {
+		const { serializer } = this.get<E>(event);
+		return serializer.serialize(event);
 	}
 
-	public getConstructor<E extends Type<IEvent>>(eventName: string): E {
-		const helper = this.get(eventName);
+	public deserializeEvent<E extends IEvent>(eventName: IEventName, payload: IEventPayload<E>): IEventInstance<E> {
+		const { serializer } = this.get<E>(eventName);
+		return serializer.deserialize(payload);
+	}
+
+	public getConstructor<E extends IEvent>(target: IEventInstance<E> | IEventName): IEventConstructor<E> {
+		const helper = this.get(target);
 		if (!helper) {
-			throw new UnregisteredEventException(eventName);
+			throw new UnregisteredEventException(target);
 		}
 
-		return helper.cls as E;
+		return helper.cls;
 	}
 
-	public getName<E extends Type<IEvent>>(cls: E): string {
-		const helper = this.get(cls);
+	public getName<E extends IEvent>(target: IEventConstructor<E> | IEventInstance<E>): IEventName {
+		const helper = this.get(target);
 		if (!helper) {
-			throw new UnregisteredEventException(cls.name);
+			throw new UnregisteredEventException(target);
 		}
 
 		return helper.name;
 	}
 
-	public getSerializer<E extends Type<IEvent>>(event: string | E): IEventSerializer<E> {
-		const helper = this.get(event);
+	public getSerializer<E extends IEvent>(target: IEventMapTarget<E>): IEventSerializer<E> {
+		const helper = this.get(target);
 		if (!helper) {
-			throw new UnregisteredEventException(typeof event === 'string' ? event : event.name);
+			throw new UnregisteredEventException(target);
 		}
 
 		return helper.serializer as IEventSerializer<E>;
