0116580c3ce80037583737ae3a3a7527b82cefbd
Dries Hooghe <dries@drieshooghe.com>
2022-09-07 14:57:26 +0200

    :recycle: make the UnregisteredEventException handle multiple input types

--- lib/exceptions/application/unregistered-event.exception.ts
+++ lib/exceptions/application/unregistered-event.exception.ts
@@ -1,5 +1,21 @@
+import { IEventMapTarget } from "../../event-map";
+
 export class UnregisteredEventException extends Error {
-	constructor(event: string) {
-		super(`Event '${event}' is not registered. Register it in the EventSourcingModule.`);
+	constructor(target: IEventMapTarget) {
+		let name: string;
+
+		switch(typeof target) {
+			case 'string':
+				name = target;
+			break;
+			case 'object':
+				name = target.constructor.name;
+			break;
+			case 'function':
+				name = target.name;
+			break;
+		}
+
+		super(`Event '${name}' is not registered. Register it in the EventSourcingModule.`);
 	}
 }
