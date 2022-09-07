f131435e2486250254c309c049de18c1e00db117
Dries Hooghe <dries@drieshooghe.com>
2022-09-07 20:55:05 +0200

    :sparkles: introduce an elasticsearch snapshot-store

---
+++ lib/integration/snapshot-store/elasticsearch.snapshot-store.spec.ts
@@ -0,0 +1,120 @@
+import { Aggregate, Id, SnapshotStream, SnapshotEnvelope } from '../../models';
+import { Client } from "@elastic/elasticsearch";
+import { ElasticsearchSnapshotEnvelopeEntity, ElasticsearchSnapshotStore } from "./elasticsearch.snapshot-store";
+import { SnapshotNotFoundException } from '../../exceptions';
+import { StreamReadingDirection } from '../../constants';
+
+class Account extends Aggregate {
+	constructor(private readonly id: AccountId, private readonly balance: number) {
+		super();
+	}
+}
+class AccountId extends Id {}
+
+describe(ElasticsearchSnapshotStore, () => {
+	let client: Client;
+	let snapshotStore: ElasticsearchSnapshotStore;
+
+	const accountId = AccountId.generate();
+	const snapshotStream = SnapshotStream.for(Account, accountId);
+
+	const snapshots = [
+		SnapshotEnvelope.new<Account>(accountId, 10, 'account', { balance: 50 }),
+		SnapshotEnvelope.new<Account>(accountId, 20, 'account', { balance: 20 }),
+		SnapshotEnvelope.new<Account>(accountId, 30, 'account', { balance: 60 }),
+		SnapshotEnvelope.new<Account>(accountId, 40, 'account', { balance: 100 }),
+		SnapshotEnvelope.new<Account>(accountId, 50, 'account', { balance: 70 }),
+		SnapshotEnvelope.new<Account>(accountId, 60, 'account', { balance: 150 }),
+	];
+
+	beforeAll(async () => {
+		client = new Client({ node: 'http://localhost:9200' });
+		snapshotStore = new ElasticsearchSnapshotStore(client);
+	});
+
+	afterEach(
+		async () =>
+			await client.deleteByQuery({
+				index: snapshotStream.subject,
+				body: { query: { match_all: {} } },
+				refresh: true,
+			}),
+	);
+
+	afterAll(async () => {
+		await client.close();
+	});
+
+	it('should append snapshots', async () => {
+		await snapshotStore.appendSnapshots(snapshotStream, snapshots);
+
+		const { body } = await client.search({
+			index: snapshotStream.subject,
+			body: { query: { match_all: {} } },
+		});
+
+		expect((body.hits.hits as ElasticsearchSnapshotEnvelopeEntity<Account>[]).map(({ _source }) => _source)).toEqual(
+			snapshots.map(({ snapshotId, ...rest }) => ({ stream: snapshotStream.name, ...rest })),
+		);
+	});
+
+	it('should retrieve a single snapshot', async () => {
+		await snapshotStore.appendSnapshots(snapshotStream, snapshots);
+
+		const resolvedSnapshot = await snapshotStore.getSnapshot(snapshotStream, snapshots[3].metadata.sequence);
+
+		expect(resolvedSnapshot).toEqual(snapshots[3]);
+	});
+
+	it("should throw when a snapshot isn't found", async () => {
+		await expect(snapshotStore.getSnapshot(snapshotStream, 5)).rejects.toThrow(
+			SnapshotNotFoundException.withVersion(snapshotStream.name, 5),
+		);
+	});
+
+	it('should retrieve snapshots forward', async () => {
+		await snapshotStore.appendSnapshots(snapshotStream, snapshots);
+
+		const resolvedSnapshots = await snapshotStore.getSnapshots(snapshotStream);
+
+		expect(resolvedSnapshots).toEqual(snapshots);
+	});
+
+	it('should retrieve snapshots backwards', async () => {
+		await snapshotStore.appendSnapshots(snapshotStream, snapshots);
+
+		const resolvedSnapshots = await snapshotStore.getSnapshots(snapshotStream, null, StreamReadingDirection.BACKWARD);
+
+		expect(resolvedSnapshots).toEqual(snapshots.slice().reverse());
+	});
+
+	it('should retrieve snapshots forward from a certain version', async () => {
+		await snapshotStore.appendSnapshots(snapshotStream, snapshots);
+
+		const resolvedSnapshots = await snapshotStore.getSnapshots(snapshotStream, 40);
+
+		expect(resolvedSnapshots).toEqual(snapshots.filter(({ metadata }) => metadata.sequence >= 40));
+	});
+
+	it('should retrieve snapshots backwards from a certain version', async () => {
+		await snapshotStore.appendSnapshots(snapshotStream, snapshots);
+
+		const resolvedSnapshots = await snapshotStore.getSnapshots(snapshotStream, 30, StreamReadingDirection.BACKWARD);
+
+		expect(resolvedSnapshots).toEqual(snapshots.filter(({ metadata }) => metadata.sequence >= 30).reverse());
+	});
+
+	it('should retrieve the last snapshot', async () => {
+		await snapshotStore.appendSnapshots(snapshotStream, snapshots);
+
+		const resolvedSnapshot = await snapshotStore.getLastSnapshot(snapshotStream);
+
+		expect(resolvedSnapshot).toEqual(snapshots[snapshots.length - 1]);
+	});
+
+	it('should return undefined if there is no last snapshot', async () => {
+		const resolvedSnapshot = await snapshotStore.getLastSnapshot(snapshotStream);
+
+		expect(resolvedSnapshot).toBeUndefined();
+	});
+});
---
+++ lib/integration/snapshot-store/elasticsearch.snapshot-store.ts
@@ -0,0 +1,108 @@
+import { Aggregate, SnapshotEnvelope, SnapshotStream } from '../../models';
+import { Client } from "@elastic/elasticsearch";
+import { ISnapshotPayload, SnapshotEnvelopeMetadata } from '../../interfaces';
+import { SnapshotNotFoundException } from '../../exceptions';
+import { SnapshotStore } from '../../snapshot-store';
+import { StreamReadingDirection } from '../../constants';
+
+export interface ElasticsearchSnapshotEnvelopeEntity<A extends Aggregate> {
+	_index: string;
+	_id: string;
+	_source: {
+		stream: string;
+		snapshotName: string;
+		payload: ISnapshotPayload<A>;
+		metadata: SnapshotEnvelopeMetadata;
+	}
+}
+
+export class ElasticsearchSnapshotStore extends SnapshotStore {
+	constructor(private readonly client: Client) {
+		super();
+	}
+
+	async getSnapshots<A extends Aggregate>(
+		{ name, subject }: SnapshotStream,
+		fromVersion?: number,
+		direction: StreamReadingDirection = StreamReadingDirection.FORWARD,
+	): Promise<SnapshotEnvelope<A>[]> {
+		const query = {
+			bool: {
+				must: [
+					{ match: { stream: name } },
+					...(fromVersion ? [{ range: { 'metadata.sequence': { gte: fromVersion } } }] : []),
+				],
+			},
+		};
+
+		const sort =
+			direction === StreamReadingDirection.FORWARD ? { 'metadata.sequence': 'asc' } : { 'metadata.sequence': 'desc' };
+
+		const { body } = await this.client.search({
+			index: subject,
+			body: { query, sort },
+		});
+
+		return body.hits.hits.map(({ _id, _source: { snapshotName, payload, metadata } }: ElasticsearchSnapshotEnvelopeEntity<A>) => {
+			return SnapshotEnvelope.from<A>(_id, snapshotName, payload, metadata);
+		});
+	}
+
+	async getSnapshot<A extends Aggregate>(
+		{ name, subject }: SnapshotStream,
+		version: number,
+	): Promise<SnapshotEnvelope<A>> {
+		const query = {
+			bool: {
+				must: [{ match: { stream: name } }, { match: { 'metadata.sequence': version } }],
+			},
+		};
+
+		const { body } = await this.client.search({
+			index: subject,
+			body: { query },
+		});
+
+		const entity: ElasticsearchSnapshotEnvelopeEntity<A> = body.hits.hits[0];
+
+		if (!entity) {
+			throw SnapshotNotFoundException.withVersion(name, version);
+		}
+
+		return SnapshotEnvelope.from<A>(entity._id, entity._source.snapshotName, entity._source.payload, entity._source.metadata);
+	}
+
+	async appendSnapshots<A extends Aggregate>(
+		{ name, subject }: SnapshotStream,
+		envelopes: SnapshotEnvelope<A>[],
+	): Promise<void> {
+		const body = envelopes.flatMap(
+			({ snapshotId, ...rest }) => [{ index: { _index: subject, _id: snapshotId } }, { stream: name, ...rest }],
+		);
+		
+		await this.client.bulk({ index: subject, body, refresh: 'wait_for' });
+	}
+
+	async getLastSnapshot<A extends Aggregate>({ name, subject }: SnapshotStream<A>): Promise<SnapshotEnvelope<A>> {
+		const query = {
+			bool: {
+				must: [{ match: { stream: name } }],
+			},
+		};
+
+		const { body } = await this.client.search({
+			index: subject,
+			body: { 
+				query,
+				sort: [{ 'metadata.sequence': 'desc' }],
+			},
+			size: 1,
+		});
+
+		const entity: ElasticsearchSnapshotEnvelopeEntity<A> = body.hits.hits[0];
+
+		if (entity) {
+			return SnapshotEnvelope.from<A>(entity._id, entity._source.snapshotName, entity._source.payload, entity._source.metadata);
+		}
+	}
+}
