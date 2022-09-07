1b02bbafb77915140305e7849963e5903ed2a7cd
Dries Hooghe <dries@drieshooghe.com>
2022-09-07 15:18:28 +0200

    :fire: remove event-listener code

--- lib/event-publisher.ts
+++ lib/event-publisher.ts
@@ -1,15 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { EventEmitter2 } from '@nestjs/event-emitter';
 import { IEvent, IEventPublisher } from './interfaces';
 
 @Injectable()
 export class EventPublisher implements IEventPublisher {
-	constructor(private readonly eventEmitter: EventEmitter2) {}
-
-	publish<T extends IEvent = IEvent>(event: T) {
-		this.eventEmitter.emit(event.constructor.name, event);
-	}
-	publishAll?<T extends IEvent = IEvent>(events: T[]) {
-		events.forEach((event) => this.publish(event));
-	}
+	publish<T extends IEvent = IEvent>(event: T) {}
+	publishAll?<T extends IEvent = IEvent>(events: T[]) {}
 }
--- lib/interfaces/events/index.ts
+++ lib/interfaces/events/index.ts
@@ -1,5 +1,4 @@
 export * from './event-envelope-metadata.interface';
-export * from './event-listener-metadata.interface';
 export * from './event-publisher.interface';
 export * from './event-serializer.interface';
 export * from './event.interface';
