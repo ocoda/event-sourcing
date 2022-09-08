734002e6c4e554038dd10e8e11c2ab980cdc9001
Dries Hooghe <dries@drieshooghe.com>
2022-09-08 08:37:22 +0200

    :fire: remove unused exception

--- lib/exceptions/application/unregistered-aggregate.exception.ts
+++
@@ -1,5 +0,0 @@
-export class UnregisteredAggregateException extends Error {
-	constructor(aggregate: string) {
-		super(`Aggregate '${aggregate}' is not registered. Register it in the EventSourcingModule.`);
-	}
-}
