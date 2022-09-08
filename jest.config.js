526f19ed9217fb10f029c737684ac342520378a6
Dries Hooghe <dries@drieshooghe.com>
2022-09-08 22:19:25 +0200

    :construction_worker: fix code coverage reports

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	moduleFileExtensions: ['js', 'json', 'ts'],
-	rootDir: 'tests',
+	rootDir: '.',
 	testRegex: '.*\\.spec\\.ts$',
 	transform: {
 		'^.+\\.(t|j)s$': [
@@ -22,10 +22,10 @@ module.exports = {
 			},
 		],
 	},
-	collectCoverageFrom: ['**/*.(t|j)s'],
-	coverageDirectory: '../coverage',
+	collectCoverageFrom: ['lib/**/*.(t|j)s'],
+	coverageDirectory: './coverage',
 	testEnvironment: 'node',
 	moduleNameMapper: {
-		'^@ocoda/event-sourcing(|/.*)$': '<rootDir>/../lib/$1',
+		'^@ocoda/event-sourcing(|/.*)$': '<rootDir>/lib/$1',
 	},
 };
