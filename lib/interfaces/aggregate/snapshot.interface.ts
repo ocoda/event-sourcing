6ea7c3c6c188ac3d3d2694089847639e9e81cb6b
Dries Hooghe <dries@drieshooghe.com>
2022-09-12 17:35:14 +0200

    :recycle: remove the snapshot payload interface, the snapshot is the payload

--- lib/interfaces/aggregate/snapshot.interface.ts
+++ lib/interfaces/aggregate/snapshot.interface.ts
@@ -5,9 +5,5 @@ type AggregatePropertyNames<T> = {
 }[keyof T];
 
 export type ISnapshot<TAggregate extends Aggregate, TDerivedAggregate = Omit<TAggregate, keyof Aggregate>> = {
-	[Key in AggregatePropertyNames<TDerivedAggregate>]: TDerivedAggregate[Key] extends Function
-		? never
-		: TDerivedAggregate[Key];
+	[Key in AggregatePropertyNames<TDerivedAggregate>]: TDerivedAggregate[Key] extends Function ? never : any;
 };
-
-export type ISnapshotPayload<TAggregate extends Aggregate> = Record<keyof ISnapshot<TAggregate>, any>;
