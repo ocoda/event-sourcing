2dac52a2cf051a6671d922aa711ec4a912f376f5
Dries Hooghe <dries@drieshooghe.com>
2022-09-13 20:22:53 +0200

    :label: add a snapshot-stream type

--- lib/interfaces/aggregate/index.ts
+++ lib/interfaces/aggregate/index.ts
@@ -1,4 +1,5 @@
 export * from './snapshot-envelope-metadata.interface';
 export * from './snapshot-handler-metadata.interface';
 export * from './snapshot-handler';
+export * from './snapshot-stream.interface';
 export * from './snapshot.interface';
