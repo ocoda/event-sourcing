10825cb4c2ce66a32cb65d457a5b8dc127debe1b
Dries Hooghe <dries@drieshooghe.com>
2022-09-13 20:16:44 +0200

    WIP

--- lib/models/event-stream.ts
+++ lib/models/event-stream.ts
@@ -1,9 +1,26 @@
 import { Type } from '@nestjs/common';
+import { IEventPool } from '../interfaces';
 import { Aggregate } from './aggregate';
 import { Id } from './id';
 
+/**
+ * @class EventStream
+ * @description EventStream is a class that represents a stream of events for a given aggregate.
+ * @example
+ * const stream = EventStream.for(Aggregate, Id);
+ * console.log(stream.name); // 'aggregate-1234'
+ * console.log(stream.subject); // 'aggregate'
+ * console.log(stream.pool); // 'default'
+ * console.log(stream.collection); // 'events'
+ */
 export class EventStream<A extends Aggregate = Aggregate> {
-	private constructor(private _subject: Type<A>, private _id: Id) {}
+	public readonly collection = 'events';
+
+	private constructor(
+		private _subject: Type<A>, 
+		private _id: Id, 
+		private _pool: IEventPool = 'default',
+	) {}
 
 	get subject(): string {
 		return this._subject.name.toLowerCase();
@@ -13,8 +30,16 @@ export class EventStream<A extends Aggregate = Aggregate> {
 		return `${this.subject}-${this._id.value}`;
 	}
 
-	static for<A extends Aggregate<any> = Aggregate<any>>(aggregate: A | Type<A>, id: Id): EventStream<A> {
+	get pool(): string | undefined {
+		return this._pool;
+	}
+
+	static for<A extends Aggregate<any> = Aggregate<any>>(
+		aggregate: A | Type<A>,
+		id: Id,
+		eventPool?: IEventPool,
+	): EventStream<A> {
 		const cls = aggregate instanceof Function ? aggregate : (aggregate.constructor as Type<A>);
-		return new EventStream<A>(cls, id);
+		return new EventStream<A>(cls, id, eventPool);
 	}
 }
--- lib/models/snapshot-stream.ts
+++ lib/models/snapshot-stream.ts
@@ -1,9 +1,12 @@
 import { Type } from '@nestjs/common';
+import { ISnapshotPool } from '../interfaces';
 import { Aggregate } from './aggregate';
 import { Id } from './id';
 
 export class SnapshotStream<A extends Aggregate = Aggregate> {
-	private constructor(private _subject: Type<A>, private _id: Id) {}
+	public readonly collection = 'snapshots';
+
+	private constructor(private _subject: Type<A>, private _id: Id, private _pool?: ISnapshotPool) {}
 
 	get subject(): string {
 		return `${this._subject.name.toLowerCase()}-snapshot`;
@@ -13,8 +16,16 @@ export class SnapshotStream<A extends Aggregate = Aggregate> {
 		return `${this.subject}-${this._id.value}`;
 	}
 
-	static for<A extends Aggregate<any> = Aggregate<any>>(aggregate: A | Type<A>, id: Id): SnapshotStream<A> {
+	get pool(): string | undefined {
+		return this._pool;
+	}
+
+	static for<A extends Aggregate<any> = Aggregate<any>>(
+		aggregate: A | Type<A>,
+		id: Id,
+		snapshotPool?: ISnapshotPool,
+	): SnapshotStream<A> {
 		const cls = aggregate instanceof Function ? aggregate : (aggregate.constructor as Type<A>);
-		return new SnapshotStream<A>(cls, id);
+		return new SnapshotStream<A>(cls, id, snapshotPool);
 	}
 }
