aba1932cb25afed08d3ff0e387af955670741baa
Dries Hooghe <dries@drieshooghe.com>
2022-09-13 20:21:15 +0200

    :label: add an event-stream type

--- lib/interfaces/events/index.ts
+++ lib/interfaces/events/index.ts
@@ -1,4 +1,5 @@
 export * from './event-envelope-metadata.interface';
 export * from './event-publisher.interface';
 export * from './event-serializer.interface';
+export * from './event-stream.interface';
 export * from './event.interface';
