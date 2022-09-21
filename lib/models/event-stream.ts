034b35272f36c7e3ac71ff787af72febf7e57315
Dries Hooghe <dries@drieshooghe.com>
2022-09-21 08:12:35 +0200

    :recycle: refactor how event- and snapshot-streams are composed

--- lib/models/event-stream.ts
+++ lib/models/event-stream.ts
@@ -1,32 +1,30 @@
 import { Type } from '@nestjs/common';
 import { AGGREGATE_METADATA } from '../decorators';
 import { MissingAggregateMetadataException } from '../exceptions';
-import { AggregateMetadata } from '../interfaces';
+import { AggregateMetadata, IEventCollection, ISnapshotPool } from '../interfaces';
 import { AggregateRoot } from './aggregate-root';
 import { Id } from './id';
 
 export class EventStream {
-	private readonly _collection = 'events';
+	private constructor(
+		private _aggregate: string,
+		private _aggregateId: string,
+		private readonly _pool?: ISnapshotPool,
+	) {}
 
-	private constructor(private _aggregate: string, private _id: string, private readonly _pool = 'default') {}
-
-	get subject(): string {
-		return `${this._aggregate}-${this._id}`;
+	get aggregateId(): string {
+		return this._aggregateId;
 	}
 
-	get collection(): string {
-		return this._collection;
+	get streamId(): string {
+		return `${this._aggregate}-${this._aggregateId}`;
 	}
 
-	get pool(): string {
-		return this._pool;
+	get collection(): IEventCollection {
+		return this._pool ? `${this._pool}-events` : 'events';
 	}
 
-	static for<A extends AggregateRoot<any> = AggregateRoot<any>>(
-		aggregate: A | Type<A>,
-		id: Id,
-		pool?: string,
-	): EventStream {
+	static for<A extends AggregateRoot = AggregateRoot>(aggregate: A | Type<A>, id: Id, pool?: string): EventStream {
 		const cls = aggregate instanceof Function ? aggregate : (aggregate.constructor as Type<A>);
 
 		const metadata: AggregateMetadata = Reflect.getMetadata(AGGREGATE_METADATA, cls);
