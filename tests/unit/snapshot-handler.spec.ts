990bb529804bee6697319e8dec303840579717d7
Dries Hooghe <dries@drieshooghe.com>
2022-09-21 20:47:39 +0200

    :white_check_mark: add a test for the snapshot-handler

---
+++ tests/unit/snapshot-handler.spec.ts
@@ -0,0 +1,109 @@
+import {
+	Aggregate,
+	AggregateRoot,
+	Id,
+	ISnapshot,
+	Snapshot,
+	SnapshotEnvelope,
+	SnapshotHandler,
+	SnapshotStore,
+	SnapshotStream,
+} from '@ocoda/event-sourcing';
+
+describe(SnapshotHandler, () => {
+	@Aggregate()
+	class Account extends AggregateRoot {
+		public id: AccountId;
+		public name: string;
+		public balance: number;
+		public openedOn: Date;
+	}
+
+	class AccountId extends Id {}
+
+	const snapshotInterval = 5;
+
+	@Snapshot(Account, { name: 'account', interval: snapshotInterval })
+	class AccountSnapshotHandler extends SnapshotHandler<Account> {
+		serialize({ id, name, balance, openedOn }: Account) {
+			return {
+				id: id.value,
+				name,
+				balance,
+				openedOn: openedOn ? openedOn.toISOString() : undefined,
+			};
+		}
+		deserialize({ id, name, balance, openedOn }: ISnapshot<Account>): Account {
+			const account = new Account();
+			account.id = AccountId.from(id);
+			account.name = name;
+			account.balance = balance;
+			account.openedOn = openedOn && new Date(openedOn);
+
+			return account;
+		}
+	}
+
+	let snapshotHandler: SnapshotHandler<Account>;
+	let account: Account;
+	let snapshot: ISnapshot<Account>;
+	let snapshotStream: SnapshotStream;
+	let snapshotEnvelope: SnapshotEnvelope<Account>;
+	let snapshotStore: jest.Mocked<SnapshotStore>;
+
+	beforeEach(() => {
+		account = new Account();
+		account.id = AccountId.generate();
+		account.name = "John Doe's Account";
+		account.balance = 100;
+		account.openedOn = new Date();
+
+		snapshotStream = SnapshotStream.for(Account, account.id);
+
+		snapshot = {
+			id: account.id.value,
+			name: account.name,
+			balance: account.balance,
+			openedOn: account.openedOn.toISOString(),
+		};
+
+		snapshotStore = {
+			appendSnapshot: jest.fn(),
+			getLastEnvelope: <any>jest.fn((stream: SnapshotStream) => Promise.resolve(snapshotEnvelope)),
+			getLastSnapshot: jest.fn(),
+			getSnapshot: jest.fn(),
+			getSnapshots: jest.fn(),
+			setup: jest.fn(),
+		};
+
+		snapshotHandler = new AccountSnapshotHandler(snapshotStore);
+	});
+
+	it('only stores snapshots in the snapshot-store at specified intervals', () => {
+		account.version = 3;
+		snapshotHandler.save(account.id, account);
+		expect(snapshotStore.appendSnapshot).not.toHaveBeenCalled();
+
+		account.version = snapshotInterval;
+		snapshotHandler.save(account.id, account);
+		expect(snapshotStore.appendSnapshot).toHaveBeenCalledWith(snapshotStream, snapshotInterval, {
+			...snapshot,
+			id: account.id.value,
+			openedOn: account.openedOn.toISOString(),
+		});
+	});
+
+	it('retrieves the latest snapshot as a snapshot-envelope', async () => {
+		account.version = snapshotInterval;
+		snapshotEnvelope = SnapshotEnvelope.create<Account>(snapshot, {
+			aggregateId: snapshotStream.aggregateId,
+			version: snapshotInterval,
+		});
+
+		const loadedAccount = await snapshotHandler.load(account.id);
+
+		expect(snapshotStore.getLastEnvelope).toHaveBeenCalledWith(snapshotStream);
+
+		expect(loadedAccount).toEqual(account);
+	});
+});
