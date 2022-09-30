ee24db0434319cca7e8cdd8cca0a1e4bfc521a3d
Dries Hooghe <dries@drieshooghe.com>
2022-09-30 19:22:56 +0200

    :fire: remove unused files

--- lib/event-bus.ts
+++
@@ -1,6 +0,0 @@
-import { Type } from '@nestjs/common';
-import 'reflect-metadata';
-import { IEvent } from './interfaces';
-
-export type EventType = Type<IEvent>;
-// export type EventListenerType = Type<IEventListener<IEvent>>;
