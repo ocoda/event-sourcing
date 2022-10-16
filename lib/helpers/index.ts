b5f35298151c8659bf0a226fd9e8a055bd8a15c2
Dries Hooghe <dries@drieshooghe.com>
2022-10-16 16:46:02 +0200

    :truck: move observable-related helpers to their own directory

--- lib/helpers/index.ts
+++ lib/helpers/index.ts
@@ -2,4 +2,4 @@ export * from './default-command-publisher';
 export * from './default-event-serializer';
 export * from './default-query-publisher';
 export * from './metadata';
-export * from './observable-bus';
+export * from './observables';
