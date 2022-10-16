a50d8f4046f2a414ef28247c54d3b9cb2863e795
Dries Hooghe <dries@drieshooghe.com>
2022-10-16 16:40:02 +0200

    🏷️ add an interface for an event-bus and event-publisher

---
+++ lib/interfaces/events/event-bus.interface.ts
@@ -0,0 +1,6 @@
+import { IEvent } from './event.interface';
+
+export interface IEventBus<EventBase extends IEvent = IEvent> {
+	publish<T extends EventBase>(event: T);
+	publishAll(events: EventBase[]);
+}
