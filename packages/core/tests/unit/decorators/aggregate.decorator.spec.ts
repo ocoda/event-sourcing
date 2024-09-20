fd2c4e4dad88fa675a76edb123bbc972e9bc9294
Dries Hooghe <dries@drieshooghe.com>
2024-09-20 10:33:58 +0200

    :sparkles: add a max length to aggregate stream names

--- packages/core/tests/unit/decorators/aggregate.decorator.spec.ts
+++ packages/core/tests/unit/decorators/aggregate.decorator.spec.ts
@@ -1,4 +1,10 @@
-import { AGGREGATE_METADATA, Aggregate, AggregateMetadata, AggregateRoot } from '@ocoda/event-sourcing';
+import {
+	AGGREGATE_METADATA,
+	Aggregate,
+	AggregateMetadata,
+	AggregateRoot,
+	InvalidAggregateStreamNameError,
+} from '@ocoda/event-sourcing';
 
 describe('@Aggregate', () => {
 	it('should add aggregate metadata to the aggregate', () => {
@@ -16,5 +22,15 @@ describe('@Aggregate', () => {
 		const { streamName }: AggregateMetadata = Reflect.getMetadata(AGGREGATE_METADATA, Account);
 
 		expect(streamName).toEqual('account');
+	});
+
+	it('should throw when an aggregate name exceeds 50 characters', () => {
+		const decorate = (length: number) => {
+			@Aggregate({ streamName: 'a'.repeat(length) })
+			class Account extends AggregateRoot {}
+		};
+
+		expect(() => decorate(50)).not.toThrow();
+		expect(() => decorate(51)).toThrow(InvalidAggregateStreamNameError.becauseExceedsMaxLength('Account', 50));
 	});
 });
