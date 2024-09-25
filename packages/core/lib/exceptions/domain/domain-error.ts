f46d0282624b82ae2c4c4438f234e251be13f4a4
Dries Hooghe <dries@drieshooghe.com>
2024-09-25 12:40:09 +0200

    📝 add JSDocs to the domain errors

--- packages/core/lib/exceptions/domain/domain-error.ts
+++ packages/core/lib/exceptions/domain/domain-error.ts
@@ -1,4 +1,10 @@
 export class DomainError extends Error {
+	/**
+	 * Creates a new instance of DomainError with the given cause.
+	 *
+	 * @param {string} cause - The cause of the domain error.
+	 * @returns {DomainError} An instance of DomainError.
+	 */
 	public static because(cause: string): DomainError {
 		return new DomainError(cause);
 	}
