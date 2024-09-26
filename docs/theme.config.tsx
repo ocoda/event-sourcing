9aac7885530124f5e480b5f985d1129a077577b4
Dries Hooghe <dries@drieshooghe.com>
2024-09-26 16:45:02 +0200

    :sparkles: add some basic documentation

---
+++ docs/theme.config.tsx
@@ -0,0 +1,21 @@
+import type { DocsThemeConfig } from 'nextra-theme-docs'
+// biome-ignore lint/correctness/noUnusedImports: <explanation>
+import React from 'react';
+
+const config: DocsThemeConfig = {
+  logo: <>
+    <img src="https://github.com/ocoda/.github/raw/master/assets/ocoda_logo_only_gradient.svg" width="40" alt="Ocoda Logo" />
+    <span style={{ marginLeft: '.4em', fontWeight: 700 }}>
+        Ocoda | Event Sourcing
+      </span>
+  </>,
+  project: {
+    link: 'https://github.com/ocoda/event-sourcing',
+  },
+  docsRepositoryBase: 'https://github.com/ocoda/event-sourcing',
+  footer: {
+    text: 'Ocoda Event Sourcing Docs',
+  },
+}
+
+export default config
