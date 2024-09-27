39e315cfdf81be0518f79ec7ab60508126b434c5
Dries Hooghe <dries@drieshooghe.com>
2024-09-27 14:01:01 +0200

    👷 fix the GITHUB_PAGES check

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,4 +1,5 @@
-const forGithubPages = process.env.GITHUB_PAGES === 'true' || false
+const forGithubPages = process.env.GITHUB_PAGES === 'true';
+console.log(`Building for Github Pages: ${forGithubPages}`);
 
 let assetPrefix = '';
 let basePath = '';
@@ -7,6 +8,8 @@ if (forGithubPages) {
   const repo = 'event-sourcing';
   assetPrefix = `/${repo}/`;
   basePath = `/${repo}`;
+  console.log(`Setting the asset prefix to: ${assetPrefix}`);
+  console.log(`Setting the base path to: ${basePath}`);
 }
 const withNextra = require('nextra')({
     theme: 'nextra-theme-docs',
