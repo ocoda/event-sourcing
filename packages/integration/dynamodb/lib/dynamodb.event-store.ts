f390e9e9be9ed05072ca7d2b7a0f36b300310558
Dries Hooghe <dries@drieshooghe.com>
2024-10-14 15:20:14 +0200

    ♻️ allow the appendEvents method to append envelopes directly

--- packages/integration/dynamodb/lib/dynamodb.event-store.ts
+++ packages/integration/dynamodb/lib/dynamodb.event-store.ts
@@ -154,7 +154,7 @@ export class DynamoDBEventStore extends EventStore<DynamoDBEventStoreConfig> {
 	async appendEvents(
 		stream: EventStream,
 		aggregateVersion: number,
-		events: IEvent[],
+		events: IEvent[] | EventEnvelope[],
 		pool?: IEventPool,
 	): Promise<EventEnvelope[]> {
 		const collection = EventCollection.get(pool);
@@ -183,6 +183,11 @@ export class DynamoDBEventStore extends EventStore<DynamoDBEventStoreConfig> {
 			const envelopes: EventEnvelope[] = [];
 			const eventIdFactory = EventId.factory();
 			for (const event of events) {
+				if (event instanceof EventEnvelope) {
+					envelopes.push(event);
+					continue;
+				}
+
 				const name = this.eventMap.getName(event);
 				const payload = this.eventMap.serializeEvent(event);
 				const envelope = EventEnvelope.create(name, payload, {
--- packages/integration/postgres/lib/postgres.event-store.ts
+++ packages/integration/postgres/lib/postgres.event-store.ts
@@ -118,7 +118,7 @@ export class PostgresEventStore extends EventStore<PostgresEventStoreConfig> {
 	async appendEvents(
 		stream: EventStream,
 		aggregateVersion: number,
-		events: IEvent[],
+		events: IEvent[] | EventEnvelope[],
 		pool?: IEventPool,
 	): Promise<EventEnvelope[]> {
 		const collection = EventCollection.get(pool);
@@ -140,6 +140,11 @@ export class PostgresEventStore extends EventStore<PostgresEventStoreConfig> {
 			const envelopes: EventEnvelope[] = [];
 			const eventIdFactory = EventId.factory();
 			for (const event of events) {
+				if (event instanceof EventEnvelope) {
+					envelopes.push(event);
+					continue;
+				}
+
 				const name = this.eventMap.getName(event);
 				const payload = this.eventMap.serializeEvent(event);
 				const envelope = EventEnvelope.create(name, payload, {
