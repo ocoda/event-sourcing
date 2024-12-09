5a068ef4e2f4c288a57e36b12d0e95efb815ac2b
Dries Hooghe <dries@drieshooghe.com>
2024-12-09 18:51:22 +0100

    :sparkles: adjust the footer and add a link about Ocoda BV

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -1,37 +1,37 @@
-
-import { useConfig, type DocsThemeConfig } from 'nextra-theme-docs'
+import { useConfig, type DocsThemeConfig } from 'nextra-theme-docs';
 
 const config: DocsThemeConfig = {
-  logo: <>
-    <img src="https://github.com/ocoda/.github/raw/master/assets/ocoda_logo_only_gradient.svg" width="40" alt="Ocoda Logo" />
-    <span style={{ marginLeft: '.4em', fontWeight: 700 }}>
-        Ocoda | Event Sourcing
-      </span>
-  </>,
-  project: {
-    link: 'https://github.com/ocoda/event-sourcing',
-  },
-  docsRepositoryBase: 'https://github.com/ocoda/event-sourcing/tree/master/docs',
-  footer: {
-    content: (
-        <span>
-          MIT {new Date().getFullYear()} ©{' '}
-          <a href="https://nextra.site" target="_blank" rel="noreferrer">
-            Ocoda
-          </a>
-          .
-        </span>
-      )
-  },
-  head: function useHead() {
-    const config = useConfig()
-    const title = `${config.title} – Ocoda Event Sourcing`
-    return (
-      <>
-        <title>{title}</title>
-      </>
-    )
-  },
-}
+	logo: (
+		<>
+			<img src="https://ocodacdn.com/image/unsafe/plain/common://ocoda_logo_gradient.svg" width="40" alt="Ocoda Logo" />
+			<span style={{ marginLeft: '.4em', fontWeight: 700 }}>Ocoda | Event Sourcing</span>
+		</>
+	),
+	project: {
+		link: 'https://github.com/ocoda/event-sourcing',
+	},
+	docsRepositoryBase: 'https://github.com/ocoda/event-sourcing/tree/master/docs',
+	footer: {
+		content: (
+			<span>
+				MIT {new Date().getFullYear()} ©{' '}
+				{/* biome-ignore lint/a11y/noBlankTarget: link to ocoda.be */}
+        <a href="https://www.ocoda.be/en" target="_blank">
+					Ocoda
+				</a>
+				.
+			</span>
+		),
+	},
+	head: function useHead() {
+		const config = useConfig();
+		const title = `${config.title} – Ocoda Event Sourcing`;
+		return (
+			<>
+				<title>{title}</title>
+			</>
+		);
+	},
+};
 
-export default config
+export default config;
